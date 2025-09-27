dcca7f74aa99290e182154bc7c0f3658f1a63d27
Ronaldo Zamora <ronaldozamora10@gmail.com>
2025-09-26 22:17:32 -0500

    xdfinal xd

---
+++ AniCLinic/frmHistorialMedicoReport.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace AniCLinic
+{
+    partial class frmHistorialMedicoReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rvwHistorial = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // rvwHistorial
+            // 
+            this.rvwHistorial.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rvwHistorial.Location = new System.Drawing.Point(0, 0);
+            this.rvwHistorial.Name = "rvwHistorial";
+            this.rvwHistorial.ServerReport.BearerToken = null;
+            this.rvwHistorial.Size = new System.Drawing.Size(546, 450);
+            this.rvwHistorial.TabIndex = 0;
+            // 
+            // frmHistorialMedicoReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 450);
+            this.Controls.Add(this.rvwHistorial);
+            this.Name = "frmHistorialMedicoReport";
+            this.Text = "frmHistorialMedicoReport";
+            this.Load += new System.EventHandler(this.frmHistorialMedicoReport_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer rvwHistorial;
+    }
+}
---
+++ AniCLinic/frmHistorialMedicoReport.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AniCLinic
+{
+    public partial class frmHistorialMedicoReport : Form
+    {
+        int idMascota;
+        public frmHistorialMedicoReport(int idMasc)
+        {
+            InitializeComponent();
+            idMascota = idMasc;
+        }
+
+        private void frmHistorialMedicoReport_Load(object sender, EventArgs e)
+        {
+            csCRUD crud = new csCRUD();
+            DataTable dt = new DataTable();
+            ReportDataSource dataset = new ReportDataSource();
+            rvwHistorial.LocalReport.DataSources.Clear();
+            dt = crud.cargarBDData(@"Select M.Nombre, R.Raza, E.Especie, M.Discapacidad, (P.Nombre + ' ' + P.Apellido) AS Propietario,
+RC.MotivoConsulta as Motivo, RC.Diagnostico, RC.Tratamiento, RC.AplicacionTratamiento as Receta,
+RC.FechaRegistro as Fecha from RegistroClinico RC
+Inner Join Mascota M ON RC.IdMascota = M.IdMascota
+Inner Join Especie E ON E.IdEspecie = M.IdEspecie
+Inner Join Raza R ON R.IdRaza = M.IdRaza
+Inner Join Persona P ON P.IdPersona = M.IdPersona 
+Where M.IdMascota = @IdMascota", new SqlParameter("@IdMascota", idMascota));
+            rvwHistorial.LocalReport.ReportEmbeddedResource = "AniCLinic.rptRegistroClinico.rdlc";
+            dataset = new ReportDataSource("dsRegistroClinico", dt);
+            rvwHistorial.LocalReport.DataSources.Add(dataset);
+            dataset.Value = dt;
+            rvwHistorial.LocalReport.Refresh();
+
+            this.rvwHistorial.RefreshReport();
+        }
+    }
+}
